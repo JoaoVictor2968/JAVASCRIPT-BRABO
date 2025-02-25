8adf3b3ccd08cf7094cce07c7ad8707e3a7330f4
JoaoVictor2968 <jvco29682968@gmail.com>
2025-02-24 22:01:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+
+// let filme = {
+//     id:1,
+//     titulo: "O senhor dos anéis: A sociedade do Anel",
+//     ano: 2001,
+//     genero: "Fantasia"
+
+// }
+
+// console.log(`O filme ${filme.titulo} do ano ${filme.ano} é o meu filme favorito`);
+
+// let estudante = {
+//     nome: "Marcão, o cara da corrida",
+//     idade: 30,
+//     curso:"corrida ao ar livre"
+// }
+// console.log(`Meu nome é ${estudante.nome}, tenho ${estudante.idade} anos de idade e estou cursando ${estudante.curso}`);
+
+// let compras = {
+//     nome: "café",
+//     preco: 450,
+//     quantidade: 3
+// }
+//  console.log(`Mãinha mandou eu ir ao mercado e comprar ${compras.quantidade} pacotes de ${compras.nome}, mas quando fui comprar percebi que os ${compras.quantidade} pacotes sairiam por ${compras.preco * compras.quantidade}, aí não deu pra comprar.`);
+
+// let usuarios = [
+//     {
+//         id:1,
+//         nome:"Zezim",
+//         cpf:89742386324,
+//         telefone:324234324
+//     },
+//     {
+//         id:2,
+//         nome:"Elias",
+//         cpf:8974233232,
+//         telefone:12342123
+//     },
+//     {
+//         id:3,
+//         nome:"Junim",
+//         cpf:48329747786,
+//         telefone:43242232
+//     }
+// ]
+
+// console.log(`${usuarios[0].nome} do cpf: ${usuarios[0].cpf} está com a matrícula travada, favor ligar para ${usuarios[0].telefone}.`);
+
+// console.log(`A oficina do ${usuarios[1].nome} está aberta hoje, ligue para ${usuarios[1].telefone} para mais informações.`);
+
+// console.log(`O ${usuarios[2].nome} do cpf: ${usuarios[2].cpf} foi sorteado no totolec, por favor venha retirar seu prêmio.`);
+
+
+// let soma = 10 + 20
+// let sub = 20 - 10
+// let mult = 2 * 20
+// let divisao = 10/2
+
+// console.log("soma:", soma);
+// console.log("subtração:", sub);
+// console.log("multiplicação:", mult);
+// console.log("divisão:", divisao);
+
+// let numero1 = prompt("Digite o primeiro número")
+// let numero2 = prompt("Digite o segundo número")
+
+// let soma = Number(numero1) + Number(numero2)
+
+// alert(soma)
+
+let nome = prompt("Digite seu nome")
+
+alert (`Bem vindo, ${nome}`)
+
+
